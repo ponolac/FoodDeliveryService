11c21ed27deff90ae3c8a5ed211e961531fa5925
ponolac <ponolac@gmail.com>
2018-12-26 23:46:40 -0800

    created a new class and selected it with jquery

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
 
+    $('.js--section-features')
 
+
+    // var waypoints = $('#handler-first').waypoint(function(direction) {
+    //     notify(this.element.id + ' hit 25% from top of window') 
+    //   }, {
+    //     offset: '25%'
+    //   })
+      
+      
 
 
 });
