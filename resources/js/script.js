8fd8268fce33f4b09709b956e119b43dc489d984
ponolac <ponolac@gmail.com>
2018-12-29 22:47:46 -0800

    jquery tested successfully, removed test code

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
 
-    $('h1').click(function() {
-        $(this).css('background-color', '#ff0000');
-    })
-
-    // $('.js--section-features')
-
+    $('.js--section-features')
 
     // var waypoints = $('#handler-first').waypoint(function(direction) {
     //     notify(this.element.id + ' hit 25% from top of window') 
